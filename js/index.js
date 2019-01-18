9f7b90b2efe6b8ee906acf33e62d87db5f878780
Arvid Nilber <aanilber@gmail.com>
2019-01-18 13:05:50 +0100

    .

---
+++ js/index.js
@@ -0,0 +1,16 @@
+AOS.init();
+smoothScroll()
+
+function smoothScroll() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+        
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+        });
+}
+
+
